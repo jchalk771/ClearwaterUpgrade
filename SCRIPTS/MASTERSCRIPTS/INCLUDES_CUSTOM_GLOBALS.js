fa59dc06f34e1bc9adc1464830ce98ac659df87e
jchalk771 <jchalk@accela.com>
2017-07-06 12:33:13 -0400

    Debug Control added

---
+++ SCRIPTS/MASTERSCRIPTS/INCLUDES_CUSTOM_GLOBALS.js
@@ -0,0 +1,7 @@
+//formerly ES_CW_DEBUGGROUPS
+//split into separate statements to support specific bizserver log debug (showDebug = 3) by individual as needed
+
+if (currentUserID == "AWILHELM") showDebug = true;
+if (currentUserID == "LMOREDA") showDebug = true;
+if (currentUserID == "JWARNER") showDebug = true;
+if (currentUserID == "MLOUIS") 	showDebug = true;
