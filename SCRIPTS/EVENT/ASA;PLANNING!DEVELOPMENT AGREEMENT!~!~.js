547550721f99b56a9f6341f8aa38aabbc994a591
jchalk771 <jchalk@accela.com>
2017-07-14 18:42:44 -0400

    TEST ME.  NOT SURE THIS WAS EXECUTING BEFORE.

---
+++ SCRIPTS/EVENT/ASA;PLANNING!DEVELOPMENT AGREEMENT!~!~.js
@@ -0,0 +1,27 @@
+//Branch
+//jec 170714 conversion begin
+
+try{
+
+	if (AInfo['Type of Case'] == "Hotel Development Agreement") {
+		//branch ("es_renbr_cap")
+		//@TODO - This may never have been executing before.  Case didn't match	"ES_RENBR_CAP". Test and document.
+		var vCap = capId;
+		var capNbr;
+		var newExt;
+		var newAltID;
+		comment("Var vCap = " + vCap);
+		capNbr = capId.getCustomID();
+		comment("Var vCap = " + capNbr);
+		newExt = capNbr.substring(3);
+		comment("Ext = " + newExt);
+		newAltID = "HDA" + newExt;
+		comment("New AltId = " + newAltID);
+		aa.cap.updateCapAltID(capId, newAltID);
+	}
+	
+}catch(err){
+	logDebug("An error occurred in ASA:Planning/Development Agreement/*/*: Conversion: " + err. message);
+	logDebug(err.stack);
+}
+//jec 170707 conversion end
