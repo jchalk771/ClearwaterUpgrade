0c6e0f2c83c840967659d81ccde228adfca41c35
jchalk771 <jchalk@accela.com>
2017-07-14 17:32:32 -0400

    ASA Branch

---
+++ SCRIPTS/EVENT/ASA;BUSINESSTAX!REGISTRATION!~!~.js
@@ -0,0 +1,14 @@
+//Branch
+//jec 170714 conversion begin
+
+try{
+
+	if (!feeExists("REGS","NEW","INVOICED")) {
+		updateFee("REGS","T_REG","FINAL",1,"N");
+	}
+
+}catch(err){
+	logDebug("An error occurred in ASA:BusinessTax/Registration/*/*: Conversion: " + err. message);
+	logDebug(err.stack);
+}
+//jec 170707 conversion end
