c71f35a68b207de62aaeddb39be88f90af349e68
jchalk771 <jchalk@accela.com>
2017-07-31 17:42:14 -0400

    email control

--- SCRIPTS/EVENT/DUA;BUILDING!~!~!~.js
+++ SCRIPTS/EVENT/DUA;BUILDING!~!~!~.js
@@ -52,8 +52,12 @@ try{
 	}
 
 	if (documentUploadedFrom == "ACA" && cap.isCompleteCap()) {
-		email("john.warner@myClearwater.com","noReply@myclearwater.com", "Online - Permit " + capId.getCustomID(), "Document was uploaded today by "+eAddress );
-		email("lydia.moreda@myClearwater.com","noReply@myclearwater.com","Uploaded - Permit " + capId.getCustomID(), "Document was uploaded today by "+eAddress );
+		//@emailTest - comment for production deployment
+			email(testMasterAddress,"noReply@myclearwater.com", "Online - Permit " + capId.getCustomID(), "Document was uploaded today by "+eAddress );
+		//@emailProd - uncomment for production deployment
+			//email("john.warner@myClearwater.com","noReply@myclearwater.com", "Online - Permit " + capId.getCustomID(), "Document was uploaded today by "+eAddress );
+			//What about this one?
+			email("lydia.moreda@myClearwater.com","noReply@myclearwater.com","Uploaded - Permit " + capId.getCustomID(), "Document was uploaded today by "+eAddress );
 	}
 
 }catch(err){
