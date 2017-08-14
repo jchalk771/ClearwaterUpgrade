21e8096fc8da24b9954fccc50f3c2272c1e3e89c
jchalk771 <jchalk@accela.com>
2017-08-14 17:35:16 -0400

    replacing aa.debug with logDebug

--- SCRIPTS/EVENT/ASA;BUILDING!~!~!~.js
+++ SCRIPTS/EVENT/ASA;BUILDING!~!~!~.js
@@ -17,25 +17,25 @@ try{
 		if (AInfo['Type of Permit'] == "A/C Change Out" && estValue > 7500 && AInfo['NOC Received']!= "CHECKED") {
 			addStdCondition("DRT Review","NOC","Not Met");
 			//aa.debug("*********************ES_ADD_NOC","Line 1");
-			aa.debug("*********************ASA:Building/*/*/*","Line 20");
+			logDebug("*********************ASA:Building/*/*/*","Line 20");
 		}
 
 		if (AInfo['Type of Permit'] == "A/C Change Out" && estValue > 7500 && AInfo['NOC Received']== "CHECKED") {
 			addStdCondition("DRT Review","NOC","Met");
 			//aa.debug("*********************ES_ADD_NOC","Line 2");
-			aa.debug("*********************ASA:Building/*/*/*","Line 26");
+			logDebug("*********************ASA:Building/*/*/*","Line 26");
 		}
 
 		if (AInfo['Type of Permit'] != "A/C Change Out" && AInfo['Type of Permit'] != "" && AInfo['Type of Permit'] != null && estValue > 2500 && AInfo['NOC Received'] != "CHECKED") {
 			addStdCondition("DRT Review","NOC","Not Met");
 			//aa.debug("*********************ES_ADD_NOC","Line 3");
-			aa.debug("*********************ASA:Building/*/*/*","Line 32");
+			logDebug("*********************ASA:Building/*/*/*","Line 32");
 		}
 
 		if (AInfo['Type of Permit'] != "A/C Change Out" && AInfo['Type of Permit'] != "" && AInfo['Type of Permit'] != null && estValue > 2500 && AInfo['NOC Received'] == "CHECKED") {
 			addStdCondition("DRT Review","NOC","Met");
 			//aa.debug("*********************ES_ADD_NOC","Line 4");
-			aa.debug("*********************ASA:Building/*/*/*","Line 38");
+			logDebug("*********************ASA:Building/*/*/*","Line 38");
 		}
 	}
 
