3fb5719bef8bbd025d00fd210c9e8fa952789092
SYSTEM <SYSTEM@TEST-ACCELA-DEV.clearwater-fl.com>
2017-09-29 09:01:47 -0400

    commit by emse tools 2017-09-29 09:01:47

--- SCRIPTS/BATCH/OCL_LATE_CASES.js
+++ SCRIPTS/BATCH/OCL_LATE_CASES.js
@@ -44,7 +44,7 @@ BTRCurrentYear = new Date().getFullYear();
 BTRCommentNote = BTRCurrentYear + "-" + (BTRCurrentYear + 1) + " BATCH";
 
 var fee;
-fee = "ADMN";
+fee = "OCT";
 
 /*------------------------------------------------------------------------------------------------------/
 | <===========Main=Loop================>
